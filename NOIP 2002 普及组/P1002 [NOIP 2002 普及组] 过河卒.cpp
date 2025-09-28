87e58ad998ab05c4da17373563901daede32d838
qingzhixing <qzsqqmail@qq.com>
2025-09-28 18:39:37 +0800

    🥞 [AC] P1002 [NOIP 2002 普及组] 过河卒

--- NOIP 2002 普及组/P1002 [NOIP 2002 普及组] 过河卒.cpp
+++ NOIP 2002 普及组/P1002 [NOIP 2002 普及组] 过河卒.cpp
@@ -1,92 +1,55 @@
 #include <iostream>
 using namespace std;
 
-struct Coord
-{
-	int x, y;
-
-	Coord operator+(const Coord &value) const
-	{
-		return {x + value.x, y + value.y};
-	}
-	Coord &operator+=(const Coord &value)
-	{
-		*this = this->operator+(value);
-		return *this;
-	}
-
-	Coord operator-(const Coord &value) const
-	{
-		return {x - value.x, y - value.y};
-	}
-
-	Coord &operator-=(const Coord &value)
-	{
-		*this = this->operator-(value);
-		return *this;
-	}
-
-	bool operator==(const Coord &value) const
-	{
-		return x == value.x && y == value.y;
-	}
-};
-
-// 下 右
-const Coord directions[2] = {{1, 0}, {0, 1}};
-
-Coord horse, target, current;
-
-int answer;
+static const int MAX_N = 30;
 
 /*
- * @return 返回当前坐标是否合格(不会被马吃掉，且没越界)
+ * road[i][j]表示从 (0,0)到(i,j)的路径数
  */
-bool check()
+long long road[MAX_N][MAX_N];
+
+int horse_x, horse_y;
+int target_x, target_y;
+
+/*
+ * @return 是否会被马吃掉或阻挡
+ */
+bool unreachable(int x, int y)
 {
-	// 越界
-	if (current.x > target.x || current.y > target.y)
-		return false;
+	int dx = abs(x - horse_x);
+	int dy = abs(y - horse_y);
 
-	int dx = current.x - horse.x;
-	int dy = current.y - horse.y;
-
-	// 被马吃掉
-	if ((abs(dx) + abs(dy) == 3) && (!(abs(dx) == 3 || abs(dy) == 3)))
-		return false;
-
-	// 跟马重合
+	// 阻挡
 	if (dx == 0 && dy == 0)
-		return false;
-	return true;
-}
+		return true;
 
-void dfs()
-{
-	if (current == target)
-	{
-		answer++;
-		return;
-	}
+	// 被吃掉
+	if ((dx + dy) == 3 && dx != 3 && dy != 3)
+		return true;
 
-	for (auto direction_index = 0; direction_index < 2; direction_index++)
-	{
-		current += directions[direction_index];
-		if (check())
-		{
-			dfs();
-		}
-		current -= directions[direction_index];
-	}
+	return false;
 }
 
 int main()
 {
-	cin >> target.x >> target.y;
-	cin >> horse.x >> horse.y;
 
-	dfs();
+	cin >> target_x >> target_y;
+	cin >> horse_x >> horse_y;
 
-	cout << answer << endl;
+	road[0][0] = 1;
+	for (int x = 0; x <= target_x; x++)
+	{
+		for (int y = 0; y <= target_y; y++)
+		{
+			if (unreachable(x, y))
+				continue;
+			if (x)
+				road[x][y] += road[x - 1][y];
+			if (y)
+				road[x][y] += road[x][y - 1];
+		}
+	}
+
+	cout << road[target_x][target_y] << endl;
 	return 0;
 }
